7ed9dbbd4a9b2c0afa79e12d5b7915471679b6a0
Luiz Felipe Bertoldi de Oliveira <lbertoldio@unisinos.br>
2019-01-09 14:44:32 -0200

    Ajustes merge request #1. Correcao das indentacoes das classes.

--- include/continental/datamanagement/rasterfile.h
+++ include/continental/datamanagement/rasterfile.h
@@ -18,112 +18,112 @@ namespace DataManagement
 /// Classe de Raster para trabalhar com dados inteiros
 /// </summary>
 template<class T>
-class CONTINENTALDATAMANAGEMENT_EXPORT RasterFile
+class RasterFile
 {
-    private:
-        //Lê um raster e retorna dados em inteiro
-        static Raster<T> getASCData(const QString &fileName, const bool onlyHeader)
+private:
+    //Lê um raster e retorna dados em inteiro
+    static Raster<T> getASCData(const QString &fileName, const bool onlyHeader)
+    {
+        const QRegularExpression regex("\\d+");
+        QFile fs(fileName);
+
+        //Lê a linha do número de colunas
+        const size_t cols = regex.match(fs.readLine()).captured(0).toULongLong();
+        //Lê a linha do número de linhas
+        const size_t rows = regex.match(fs.readLine()).captured(0).toULongLong();
+        //Lê a linha da coordenada inferior em X
+        const double xOrigin = regex.match(fs.readLine()).captured(0).toDouble();
+        //Lê a linha da coordenada inferior em Y
+        const double yOrigin = regex.match(fs.readLine()).captured(0).toDouble();
+        //Lê a linha do cellsize
+        const double cellSize = regex.match(fs.readLine()).captured(0).toDouble();
+        //Lê a linha do NODATA
+        const int noData = regex.match(fs.readLine()).captured(0).toInt();
+
+        Raster<T> raster(rows, cols, xOrigin, yOrigin, cellSize, noData);
+
+        //Lê apenas as informações do cabeçalho, caso ativado
+        if (onlyHeader)
         {
-            const QRegularExpression regex("\\d+");
-            QFile fs(fileName);
-
-            //Lê a linha do número de colunas
-            const size_t cols = regex.match(fs.readLine()).captured(0).toULongLong();
-            //Lê a linha do número de linhas
-            const size_t rows = regex.match(fs.readLine()).captured(0).toULongLong();
-            //Lê a linha da coordenada inferior em X
-            const double xOrigin = regex.match(fs.readLine()).captured(0).toDouble();
-            //Lê a linha da coordenada inferior em Y
-            const double yOrigin = regex.match(fs.readLine()).captured(0).toDouble();
-            //Lê a linha do cellsize
-            const double cellSize = regex.match(fs.readLine()).captured(0).toDouble();
-            //Lê a linha do NODATA
-            const int noData = regex.match(fs.readLine()).captured(0).toInt();
-
-            Raster<T> raster(rows, cols, xOrigin, yOrigin, cellSize, noData);
-
-            //Lê apenas as informações do cabeçalho, caso ativado
-            if (onlyHeader)
+            size_t position = 0;
+            for (size_t i = 0; i < rows; ++i)
             {
-                size_t position = 0;
-                for (size_t i = 0; i < rows; ++i)
+                const auto line = fs.readLine().split(' ');
+                for (size_t j = 0; j < cols; ++j)
                 {
-                    const auto line = fs.readLine().split(' ');
-                    for (size_t j = 0; j < cols; ++j)
-                    {
-                        raster.setData(position, static_cast<T>(line[j].toFloat()));
-                        ++position;
-                    }
+                    raster.setData(position, static_cast<T>(line[j].toFloat()));
+                    ++position;
                 }
             }
-
-            return raster;
         }
 
-        /// <summary>
-        /// Escreve os dados da matriz para um arquivo ASC especificado.
-        /// </summary>
-        /// <param name="ArquivoDestino"></param>
-        const static void writeASCData(const Raster<T> &raster, const QString &fileName)
+        return raster;
+    }
+
+    /// <summary>
+    /// Escreve os dados da matriz para um arquivo ASC especificado.
+    /// </summary>
+    /// <param name="ArquivoDestino"></param>
+    const static void writeASCData(const Raster<T> &raster, const QString &fileName)
+    {
+        QFile fs(fileName);
+        if (!fs.open(QIODevice::WriteOnly | QIODevice::Text))
         {
-            QFile fs(fileName);
-            if (!fs.open(QIODevice::WriteOnly | QIODevice::Text))
-            {
-                throw std::invalid_argument("Error openning file!");
-            }
-
-            QTextStream out(&fs);
-
-            const QString lineBreaker("\n");
-            const QString space(" ");
-
-            out << "ncols " << raster.getCols() << lineBreaker;
-            out << "nrows " << raster.getRows() << lineBreaker;
-            out << "xllcorner " << raster.getXOrigin() << lineBreaker;
-            out << "yllcorner " << raster.getYOrigin() << lineBreaker;
-            out << "cellsize " << raster.getCellSize() << lineBreaker;
-            out << "NODATA_value " << raster.getNoDataValue() << lineBreaker;
-
-            const size_t cols = raster.getCols();
-            const size_t limit = raster.getRows() * cols;
-            for (size_t i = 0; i < limit; ++i)
-            {
-                out << raster.getData(i) << ((i + 1) % cols == 0 ? lineBreaker : space);
-            }
+            throw std::invalid_argument("Error openning file!");
         }
 
-    public:
-        /// <summary>
-        /// Lê os dados a partir de um arquivo extero
-        /// </summary>
-        static Raster<T> loadRasterByFile(const QString &fileName, const bool onlyHeader = false)
+        QTextStream out(&fs);
+
+        const QString lineBreaker("\n");
+        const QString space(" ");
+
+        out << "ncols " << raster.getCols() << lineBreaker;
+        out << "nrows " << raster.getRows() << lineBreaker;
+        out << "xllcorner " << raster.getXOrigin() << lineBreaker;
+        out << "yllcorner " << raster.getYOrigin() << lineBreaker;
+        out << "cellsize " << raster.getCellSize() << lineBreaker;
+        out << "NODATA_value " << raster.getNoDataValue() << lineBreaker;
+
+        const size_t cols = raster.getCols();
+        const size_t limit = raster.getRows() * cols;
+        for (size_t i = 0; i < limit; ++i)
         {
-            const QRegularExpression regexAsc("\\.asc$", QRegularExpression::CaseInsensitiveOption);
-
-            //Extensão .ASC
-            if (regexAsc.match(fileName).hasMatch())
-                return RasterFile<T>::getASCData(fileName, onlyHeader);
-            else
-                throw std::invalid_argument("Selected data format is not compatible!");
+            out << raster.getData(i) << ((i + 1) % cols == 0 ? lineBreaker : space);
         }
+    }
 
-        /// <summary>
-        /// Escreve os dados no disco
-        /// </summary>
-        const static void writeData(const Raster<T> &raster, const QString &fileName)
+public:
+    /// <summary>
+    /// Lê os dados a partir de um arquivo extero
+    /// </summary>
+    static Raster<T> loadRasterByFile(const QString &fileName, const bool onlyHeader = false)
+    {
+        const QRegularExpression regexAsc("\\.asc$", QRegularExpression::CaseInsensitiveOption);
+
+        //Extensão .ASC
+        if (regexAsc.match(fileName).hasMatch())
+            return RasterFile<T>::getASCData(fileName, onlyHeader);
+        else
+            throw std::invalid_argument("Selected data format is not compatible!");
+    }
+
+    /// <summary>
+    /// Escreve os dados no disco
+    /// </summary>
+    const static void writeData(const Raster<T> &raster, const QString &fileName)
+    {
+        const QRegularExpression regexAsc("\\.asc$", QRegularExpression::CaseInsensitiveOption);
+
+        //Extensão .ASC
+        if (regexAsc.match(fileName).hasMatch())
         {
-            const QRegularExpression regexAsc("\\.asc$", QRegularExpression::CaseInsensitiveOption);
-
-            //Extensão .ASC
-            if (regexAsc.match(fileName).hasMatch())
-            {
-                RasterFile<T>::writeASCData(raster, fileName);
-            }
-            else
-            {
-                throw std::invalid_argument("Selected data format is not compatible!");
-            }
+            RasterFile<T>::writeASCData(raster, fileName);
         }
+        else
+        {
+            throw std::invalid_argument("Selected data format is not compatible!");
+        }
+    }
 };
 }
 }
--- include/continental/datamanagement/spatialanalyst.h
+++ include/continental/datamanagement/spatialanalyst.h
@@ -16,47 +16,47 @@ namespace DataManagement
 {
 class CONTINENTALDATAMANAGEMENT_EXPORT SpatialAnalyst
 {
-    public:
-        static constexpr double EARTH_SEMI_MAJOR_AXIS = 6378.137;
-        static constexpr double EARTH_SEMI_MINOR_AXIS = 6356.752;
+public:
+    static constexpr double EARTH_SEMI_MAJOR_AXIS = 6378.137;
+    static constexpr double EARTH_SEMI_MINOR_AXIS = 6356.752;
 
-        //Realiza a operação de Reclass, com base nos parâmetros específicados
-        template<class T>
-        void reclassRaster(Raster<T> &rasterToReclass, const std::vector<ReclassParameters> &parameters)
+    //Realiza a operação de Reclass, com base nos parâmetros específicados
+    template<class T>
+    void reclassRaster(Raster<T> &rasterToReclass, const std::vector<ReclassParameters> &parameters)
+    {
+        const size_t limit = rasterToReclass.getRows() * rasterToReclass.getCols();
+        for (size_t position = 0; position < limit; ++position)
         {
-            const size_t limit = rasterToReclass.getRows() * rasterToReclass.getCols();
-            for (size_t position = 0; position < limit; ++position)
-            {
-                for (const auto &parameter : parameters) {
-                    if (rasterToReclass.getData(position) >= parameter.getLowerBound() && rasterToReclass->getData(position) < parameter.getUpperBound())
-                    {
-                        rasterToReclass.setData(position, parameter.getNewValue());
-                        break;
-                    }
+            for (const auto &parameter : parameters) {
+                if (rasterToReclass.getData(position) >= parameter.getLowerBound() && rasterToReclass->getData(position) < parameter.getUpperBound())
+                {
+                    rasterToReclass.setData(position, parameter.getNewValue());
+                    break;
                 }
             }
         }
+    }
 
-        static double areaCell(const size_t row, const size_t column, const double xOrigin, const double yOrigin, const double cellSize, const size_t rows, const size_t cols);
-        static double cellLength(const double yLatitude, const double xLongitude, const short relY, const short relX, const double cellSize);
+    static double areaCell(const size_t row, const size_t column, const double xOrigin, const double yOrigin, const double cellSize, const size_t rows, const size_t cols);
+    static double cellLength(const double yLatitude, const double xLongitude, const short relY, const short relX, const double cellSize);
 
-        //Extrair um raster a partir de outro
-        template<class T>
-        Raster<T> extractRasterByRaster(Raster<T> &bigRaster, const Raster<T> &referenceRaster)
+    //Extrair um raster a partir de outro
+    template<class T>
+    Raster<T> extractRasterByRaster(Raster<T> &bigRaster, const Raster<T> &referenceRaster)
+    {
+        //Compatibiliza as informações do raster grande, com o espaço do raster pequeno
+        ResampleData::adjustSpatialData(bigRaster, referenceRaster);
+
+        const auto limit = referenceRaster.getRows() * referenceRaster.getCols();
+        for (size_t position = 0; position < limit; ++position)
         {
-            //Compatibiliza as informações do raster grande, com o espaço do raster pequeno
-            ResampleData::adjustSpatialData(bigRaster, referenceRaster);
-
-            const auto limit = referenceRaster.getRows() * referenceRaster.getCols();
-            for (size_t position = 0; position < limit; ++position)
-            {
-                //Extrai a informação somente para o intervalo onde houverem valores para o raster de referência
-                if (referenceRaster.getData(position) != referenceRaster.getNoDataValue())
-                    referenceRaster.setData(position, bigRaster.getData(position));
-            }
-
-            return referenceRaster;
+            //Extrai a informação somente para o intervalo onde houverem valores para o raster de referência
+            if (referenceRaster.getData(position) != referenceRaster.getNoDataValue())
+                referenceRaster.setData(position, bigRaster.getData(position));
         }
+
+        return referenceRaster;
+    }
 };
 }
 }
